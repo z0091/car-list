fcac5f0b7b233318afea13618292bf593a45c9db
Alexander Plotnikov <p.a.ig.rus@gmail.com>
2017-07-17 00:48:13 +0400

    Added element-ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(ElementUI);
 
 new Vue({ // eslint-disable-line no-new
     el: '#app',
